4f861d6a44fbc6e34cda8a418f5bf4fdc0f70ea4
wuriko <noahfenghom@gmail.com>
2026-02-15 12:30:26 +0100

    feat: improve documentation

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,14 @@
 import deckyPlugin from "@decky/rollup";
+import replace from "@rollup/plugin-replace";
+import {readFileSync} from "fs";
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default deckyPlugin({
-  // Add your extra Rollup options here
+    plugins: [
+        replace({
+            preventAssignment: false,
+            "process.env.HOMEPAGE": JSON.stringify(pkg.homepage),
+        }),
+    ],
 })
